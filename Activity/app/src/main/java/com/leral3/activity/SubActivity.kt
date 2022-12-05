247eedc36233a575524184e14c30811721e4b64d
leral3 <leral3@naver.com>
2022-12-05 15:19:30 +0900

    send the value to other SubActivity.kt

---
+++ Activity/app/src/main/java/com/leral3/activity/SubActivity.kt
@@ -0,0 +1,20 @@
+package com.leral3.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leral3.activity.databinding.ActivitySubBinding
+
+class SubActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivitySubBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.to1.text = intent.getStringExtra("from1")
+        binding.to2.text = "${ intent.getIntExtra("from2",0) }"
+        binding.to3.text = intent.getStringExtra("from3")
+
+    }
+}
