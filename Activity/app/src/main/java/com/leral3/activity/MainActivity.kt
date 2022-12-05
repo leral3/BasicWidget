8e0a249d56bba1624a3850140a1627968a491ddb
leral3 <leral3@naver.com>
2022-12-05 15:30:54 +0900

    reply the value to MainActivity.kt

---
+++ Activity/app/src/main/java/com/leral3/activity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.leral3.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.leral3.activity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val activityResult = registerForActivityResult(ActivityResultContracts. StartActivityForResult()) {
+            if(it.resultCode == RESULT_OK) {
+                val message = it.data?.getStringExtra("returnValue")
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            intent.putExtra("from1", "Hello Bundle")
+            intent.putExtra("from2", 2022)
+            intent.putExtra("from3", binding.editMessage.text.toString())
+
+            activityResult.launch(intent)
+        }
+
+
+    }
+}
--- Activity/app/src/main/java/com/leral3/activity/SubActivity.kt
+++ Activity/app/src/main/java/com/leral3/activity/SubActivity.kt
@@ -1,5 +1,6 @@
 package com.leral3.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.leral3.activity.databinding.ActivitySubBinding
@@ -15,6 +16,13 @@ class SubActivity : AppCompatActivity() {
         binding.to1.text = intent.getStringExtra("from1")
         binding.to2.text = "${ intent.getIntExtra("from2",0) }"
         binding.to3.text = intent.getStringExtra("from3")
+
+        binding.btnClose.setOnClickListener {
+            val returnIntent = Intent()
+            returnIntent.putExtra("returnValue", binding.editMessageSub.text.toString())
+            setResult(RESULT_OK, returnIntent)
+            finish()
+        }
 
     }
 }
