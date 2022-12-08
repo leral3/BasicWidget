7ff8d9369b327409f744146dbc9800e8889eb94c
leral3 <leral3@naver.com>
2022-12-08 11:35:38 +0900

    Add to Applied Fragment

---
+++ Fragment_1_3/app/src/main/java/com/ReceiverFragment.kt
@@ -0,0 +1,35 @@
+package com
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import com.leral3.fragment_1_3.R
+import com.leral3.fragment_1_3.databinding.FragmentReceiverBinding
+
+
+class ReceiverFragment : Fragment() {
+
+    lateinit var binding: FragmentReceiverBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentReceiverBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFragmentResultListener("request") {key, bundle ->
+            bundle.getString("valueKey")?.let {
+                binding.textView.setText(it)
+            }
+        }
+    }
+}
